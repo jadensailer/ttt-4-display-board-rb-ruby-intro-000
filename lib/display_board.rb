bb43afc76c9a754d273330c6eeb6c86280da2b17
Jaden Sailer <jadennsailer@gmail.com>
2020-08-29 00:05:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,10 @@
 # out the current state.
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
